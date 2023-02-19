612a0af8cbdc74d2f804f9ea63b919b73efcd2fb
taisei miyaji <taisei.miyaji4602@gmail.com>
2023-02-19 16:55:33 +0900

    update: make footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,15 @@ import './Footer.scss';
 
 export default function Footer() {
     return (
-        <div></div>
+        <div className="footer">
+            <div className="copyright">©︎ 2023 taisei miyaji</div>
+            <div className="icons">
+                <span className="github">
+                    <a href="https://github.com/taiseimiyaji" target="_blank">
+                        <img src="/dist/github-mark/github-mark-white.png" width="25" height="25"></img>
+                    </a>
+                </span>
+                </div>
+        </div>
     );
 }
