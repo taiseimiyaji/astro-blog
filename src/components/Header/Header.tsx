872f5a8a9217e5af9fb7a73761d9f57cfdf5d19a
taisei miyaji <taisei.miyaji4602@gmail.com>
2023-03-18 18:04:24 +0900

    プロフィールの追加

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,16 @@ import "./Header.scss";
 export default function Header() {
     return (
         <div className="header">
-            <div className="title">Lyricrime.com</div>
+            <div className="title">
+                <a href="/">Lyricrime.com</a></div>
             <div className="header-link">
                 <a href="/">
                     <div> Blog</div>
                 </a>
                 <div>Activity</div>
-                <div>SNS</div>
+                <a href="/profile">
+                    <div>Profile</div>
+                </a>
             </div>
         </div>
     );
