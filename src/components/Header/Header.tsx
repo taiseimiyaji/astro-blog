975fa3aba63384776f43b9439d11f9818b513907
taisei miyaji <taisei.miyaji4602@gmail.com>
2023-07-08 12:57:01 +0900

    add: Activity

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,9 @@ export default function Header() {
                 <a href="/">
                     <div> Blog</div>
                 </a>
-                <div>Activity</div>
+                <a href="/activity">
+                    <div>Activity</div>
+                </a>
                 <a href="/profile">
                     <div>Profile</div>
                 </a>
